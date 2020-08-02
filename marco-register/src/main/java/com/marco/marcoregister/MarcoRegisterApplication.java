294448682068359d1dc9e4996e3d6b45de1d3bb6
iamwumaixing <841240993@qq.com>
2020-08-03 01:34:50 +0800

    init

---
+++ marco-register/src/main/java/com/marco/marcoregister/MarcoRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.marco.marcoregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcoRegisterApplication.class, args);
+    }
+
+}
